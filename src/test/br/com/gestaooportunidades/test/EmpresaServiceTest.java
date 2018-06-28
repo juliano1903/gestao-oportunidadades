0e54179d9eaabff6e11d58477299c3cfdde7e6fe
Juliano lara <llara.juliano@gmail.com>
2018-06-28 11:27:23 -0300

    COnfiguração para testes unitários.

---
+++ src/test/br/com/gestaooportunidades/test/EmpresaServiceTest.java
@@ -0,0 +1,19 @@
+package br.com.gestaooportunidades.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.gestaooportunidades.Configuracao;
+
+@RunWith(SpringRunner.class)
+@SpringApplicationConfiguration(classes = Configuracao.class)
+public class EmpresaServiceTest {
+
+	@Test
+	public void test() {
+		
+	}
+}
